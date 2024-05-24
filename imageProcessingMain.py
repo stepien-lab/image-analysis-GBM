2f3da14c273668c6a0be32a4f49119a947e0159d
gilliancarr <150491809+gilliancarr@users.noreply.github.com>
2024-05-24 11:52:00 -0400

    Update imageProcessingMain.py

--- imageProcessingMain.py
+++ imageProcessingMain.py
@@ -1,14 +1,14 @@
 from imageProcessingFunctions import *
 
 
-# specify spreadsheet and plots output folder
+# Specify spreadsheet and plots output folder
 data_folder = "/Users/gillian/Desktop/UF/Thesis/Spreadsheets/"
 plot_folder = "/Users/gillian/Desktop/UF/Thesis/Plots/"
 
-# import mouse ids and section numbers from spreadsheet
+# Import mouse ids and section numbers from spreadsheet
 mouse_section_id = pd.read_csv('mouse_sections.csv', usecols=['Mouse_Sections'])
 
-# run analysis workflow for each image set (19 cross-sectional tumor image sets, three cell types per set)
+# Run analysis workflow for each image set (19 cross-sectional tumor image sets, three cell types per set)
 for index, row in mouse_section_id.iterrows():
     image_set(data_folder, plot_folder, row['Mouse_Sections'])
 
