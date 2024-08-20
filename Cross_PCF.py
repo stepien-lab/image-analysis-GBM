b9e297332f4fc6ebdc0a7aa1b5e253c5646549b4
gilliancarr <150491809+gilliancarr@users.noreply.github.com>
2024-08-20 18:11:08 -0400

    Update Cross_PCF.py

--- Cross_PCF.py
+++ Cross_PCF.py
@@ -17,39 +17,57 @@ sns.set(font_scale=5)
 # create data frame of mouse ID strings
 mouse_section_id = pd.read_csv('mouse_sections.csv', usecols=['Mouse_Sections'])
 
-# import x and y locations of cancer cells from CellProfiler .csv file, convert from pixels to micrometers (0.62 um/px)
-cancer_data = pd.read_csv(mouse_section_id.loc[0, 'Mouse_Sections']+'_Data_CancerCells.csv', usecols=['Location_Center_X', 'Location_Center_Y'])
+# choose image set for analysis
+id_index = 0
+
+#%% format data for cross-PCF
+
+# import x and y locations of cancer cells from CellProfiler .csv file,
+# convert from pixels to micrometers (0.62 um/px)
+cancer_data = pd.read_csv(mouse_section_id.loc[id_index, 'Mouse_Sections']+'_Data_CancerCells.csv', usecols=['Location_Center_X', 'Location_Center_Y'])
 cancer_data_um = cancer_data.mul(0.62)
 cancer_array = cancer_data_um.to_numpy()
 
 # import x and y locations of MDSCs from CellProfiler .csv file, convert from pixels to micrometers (0.62 um/px)
-mdsc_data = pd.read_csv(mouse_section_id.loc[0, 'Mouse_Sections'] + '_Data_MDSC.csv', usecols=['Location_Center_X', 'Location_Center_Y'])
+mdsc_data = pd.read_csv(mouse_section_id.loc[id_index, 'Mouse_Sections'] + '_Data_MDSC.csv', usecols=['Location_Center_X', 'Location_Center_Y'])
 mdsc_data_um = mdsc_data.mul(0.62)
 mdsc_array = mdsc_data_um.to_numpy()
 
 # import x and y locations of T-cells from CellProfiler .csv file, convert from pixels to micrometers (0.62 um/px)
-cd3_data = pd.read_csv(mouse_section_id.loc[0, 'Mouse_Sections'] + '_Data_CD3.csv', usecols=['Location_Center_X', 'Location_Center_Y'])
-cd3_data_um = cd3_data.mul(0.62)
-cd3_array = cd3_data_um.to_numpy()
+t_cell_data = pd.read_csv(mouse_section_id.loc[id_index, 'Mouse_Sections'] + '_Data_CD3.csv', usecols=['Location_Center_X', 'Location_Center_Y'])
+t_cell_data_um = t_cell_data.mul(0.62)
+t_cell_array = t_cell_data_um.to_numpy()
 
 # combine cell types into one array for later use in cross-PCFs
-cancer_mdsc = arr = np.vstack((cancer_array, mdsc_array))
+cancer_mdsc = arr1 = np.vstack((cancer_array, mdsc_array))
+cancer_t_cell = arr2 = np.vstack((cancer_array, t_cell_array))
+mdsc_t_cell = arr3 = np.vstack((mdsc_array, t_cell_array))
+cancer_cancer = arr4 = np.vstack((cancer_array, cancer_array))
+t_cell_t_cell = arr5 = np.vstack((t_cell_array, t_cell_array))
+mdsc_mdsc = arr6 = np.vstack((mdsc_array, mdsc_array))
 
 # create array of cell labels for later use in cross-PCFs
-labs = np.concatenate((['Cancer Cells']*cancer_array.shape[0], ['MDSCs']*mdsc_array.shape[0]))
+labs_cancer_mdsc = np.concatenate((['Cancer Cells']*cancer_array.shape[0], ['MDSCs']*mdsc_array.shape[0]))
+labs_cancer_t_cell = np.concatenate((['Cancer Cells']*cancer_array.shape[0], ['T-cells']*t_cell_array.shape[0]))
+labs_mdsc_t_cell = np.concatenate((['MDSCs']*mdsc_array.shape[0], ['T-cells']*t_cell_array.shape[0]))
+labs_cancer_cancer = np.concatenate((['Cancer Cells']*cancer_array.shape[0], ['Cancer Cells']*cancer_array.shape[0]))
+labs_t_cell_t_cell = np.concatenate((['T-cells']*t_cell_array.shape[0], ['T-cells']*t_cell_array.shape[0]))
+labs_mdsc_mdsc = np.concatenate((['MDSCs']*mdsc_array.shape[0], ['MDSCs']*mdsc_array.shape[0]))
 
+# choose one comparison to run
+labs = labs_cancer_mdsc
+
+#%% plot cell types
 pc = generatePointCloud(mouse_section_id, cancer_mdsc)
 pc.addLabels('Celltype','categorical',labs,cmap='tab10')
-#pc.addLabels('$m$','continuous',contuslabs,cmap='RdBu_r')
 
 visualisePointCloud(pc,'Celltype',markerSize=100)#,showBoundary=True)
-#visualisePointCloud(pc,'$m$',markerSize=100,cmap='Oranges')#,showBoundary=True)
 c1mask = np.asarray(labs) == 'Cancer Cells'
 plt.scatter(cancer_mdsc[c1mask,0],cancer_mdsc[c1mask,1],s=100,zorder=-1)
 plt.gca().invert_yaxis()
 plt.show()
 
-#%% Calculate cross-PCFs
+#%% calculate cross-PCFs
 a = 'Cancer Cells'
 b = 'MDSCs'
 maxR=1000
@@ -65,9 +83,11 @@ plt.plot(r2,pcf2,lw=7,label='$g_{MDSCs Cancer}(r)$',linestyle=(0,(1,1.5)))
 plt.xlabel(r'Radius, $r$ ($\mu$m)')
 plt.ylim([0,7])
 plt.legend()
-plt.show() # I added this to see plots
+plt.show() # added this to see plots
 
-#%% Calculate TCMs
+print('r' + r + 'pcf' + pcf + 'contributions' + contributions)
+
+#%% calculate TCMs
 tcm = topographicalCorrelationMap(pc,'Celltype','Cancer Cells','Celltype','MDSCs',radiusOfInterest=50,maxCorrelationThreshold=5.0,kernelRadius=150,kernelSigma=50,visualiseStages=False)
 
 plt.figure(figsize=(20,20))
@@ -77,7 +97,7 @@ plt.colorbar(label=r'$\Gamma_{Cancer MDSCs}(r=50)$')
 ax = plt.gca()
 ax.grid(False)
 plt.gca().invert_yaxis()
-plt.show() # I added this to see plots
+plt.show() # added this to see plots
 
 tcm = topographicalCorrelationMap(pc,'Celltype','MDSCs','Celltype','Cancer Cells',radiusOfInterest=50,maxCorrelationThreshold=5.0,kernelRadius=150,kernelSigma=50,visualiseStages=False)
 
@@ -88,4 +108,4 @@ plt.colorbar(label=r'$\Gamma_{MDCSs Cancer}(r=50)$')
 ax = plt.gca()
 ax.grid(False)
 plt.gca().invert_yaxis()
-plt.show() # I added this to see plots
+plt.show() # added this to see plots
